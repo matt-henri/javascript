c1352076dde8622eddfea60d93e3719c29f2d997
Mateus Henrique <mathmarkub@gmail.com>
2022-11-30 10:43:38 -0300

    Update ex009.js

---
+++ aula04/ex009.js
@@ -0,0 +1,11 @@
+//   ========= Condição Simples =========
+
+
+
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log("Você é Brasileiro!")
+} else {
+    console.log (`Você é Estangeiro!`)
+}
