3f7d9a44c4ba5e8e5f50009e3823f46e799be693
Mateus Henrique <mathmarkub@gmail.com>
2022-12-07 17:33:22 -0300

    projeto com js

--- aula06-desafio/modelo02/script.js
+++ aula06-desafio/modelo02/script.js
@@ -3,5 +3,47 @@ function verificar() {
    var ano = data.getFullYear()
    var fano = document.getElementById('txtano')
    var res = document.querySelector('div#res')
-   if (fano.value.length == 0 || fano.value > ano)
+   if (fano.value.length == 0 || fano.value > ano) {
+      window.alert('[ERRO] Verificar os dados e tente novamente!')
+   } else {
+      var fsex = document.getElementsByName('radsex')
+      var idade = ano - Number(fano.value)
+      var gênero = ''
+      var img = document.createElement('img')
+      img.setAttribute('id', 'foto')
+      if (fsex[0].checked) {
+         gênero = 'Homem'
+         if  (idade >= 0 && idade < 10) {
+            // criança
+            img.setAttribute('src', './imagens/menino.png')
+         } else if (idade >= 18 && idade < 25) {
+            //  jovem
+            img.setAttribute('src', './imagens/jovemh.png')
+         } else if (idade < 50){
+            // adulto
+            img.setAttribute('src', './imagens/adulto.png')
+         } else {
+            // idoso
+            img.setAttribute('src', './imagens/idoso.png')
+         }
+      } else if (fsex[1].checked) {
+         gênero = 'Mulher'
+         if  (idade >= 0 && idade < 10) {
+            // criança
+            img.setAttribute('src', './imagens/menina.png')
+         } else if (idade >= 18 && idade < 25) {
+            //  jovem
+            img.setAttribute('src', './imagens/jovemm.png')
+         } else if (idade >= 27 && idade < 50){
+            // adulto
+            img.setAttribute('src', './imagens/adulta.png')
+         } else {
+            // idoso
+            img.setAttribute('src', './imagens/idosa.png')
+         }
+      }
+      res.style.textAlign = 'center'
+      res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+      res.appendChild(img)
+   }
 }
