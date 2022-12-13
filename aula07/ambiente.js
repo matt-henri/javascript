12e443a6a2e18fa223de6d0038ecf7737808ed45
Mateus Henrique <mathmarkub@gmail.com>
2022-12-13 11:43:43 -0300

    atualizando estudo de js

---
+++ aula07/ambiente.js
@@ -0,0 +1,18 @@
+/*
+
+            var c = 1
+                while (c <= 10) {
+                console.log(c)
+                c++
+            }
+
+*/
+
+
+// ----
+
+console.log('Vai começar...')
+for(var c = 1;c <= 12; c++) {
+    console.log (c)
+}
+console.log('FIM!')
