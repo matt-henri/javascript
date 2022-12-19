d1bf87addbb3898a4e833b33fa010340a45205a1
Mateus Henrique <mathmarkub@gmail.com>
2022-12-19 11:02:48 -0300

    estudo básico de JavaScript

---
+++ OBS/001.js
@@ -0,0 +1,17 @@
+var numero = 5;
+
+console.log(numero)
+console.log(typeof numero)
+
+
+var float = 5.2
+
+console.log(float)
+console.log(typeof float)
+
+
+var textoComNumero = '532'
+
+console.log(textoComNumero)  // reconhece como numero
+console.log(typeof textoComNumero)  //string
+
