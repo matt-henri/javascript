460f81d903adce9e203cd3689e78485d076f1a5a
Mateus Henrique <mathmarkub@gmail.com>
2022-12-19 12:42:47 -0300

    Create 004-boolean.js

---
+++ OBS/004-boolean.js
@@ -0,0 +1,12 @@
+var idade = 18
+
+if (idade >= 18) {
+    console.log('Pode fazer a carteira de habilitação.')
+}
+
+
+if(true) {
+    console.log('testando o if')
+}
+
+// caso for 'false' o console ignora a etapa e segue em frente
