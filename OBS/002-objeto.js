f5e4ed8ef1ec88361dcad93da451e554ab2b9a12
Mateus Henrique <mathmarkub@gmail.com>
2022-12-19 12:08:13 -0300

    estudo de js basico

---
+++ OBS/002-objeto.js
@@ -0,0 +1,15 @@
+var obj = {
+    nome: "Mateus",
+    idade: "27",
+    profissao: "Programador"
+};
+
+console.log(obj) // apresenta todos os dados dentro do obj
+console.log(obj.nome) //entraga apenas o dado específico
+console.log('Meu nome é ' + obj.nome) 
+
+// Para adicionar nova propriedade ao objeto
+
+obj.graduacao = true
+
+console.log(obj)
---
+++ OBS/003-array.js
@@ -0,0 +1,17 @@
+var arr = [5, "Mateus", true, {teste: 1, teste: 2}] //obj => {}
+
+console.log(arr)
+
+var arr2 = [2,3,4,5,6]
+
+console.log(arr2)
+
+console.log(arr[1])
+console.log(arr2[0])
+
+arr[4] = 10
+
+console.log(arr)
+
+arr2[5] = 7
+console.log(arr2)
