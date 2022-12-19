61936faa3f619b301bc21f49c85f01cd68add3ae
Mateus Henrique <mathmarkub@gmail.com>
2022-12-19 11:05:07 -0300

    Update 001.js

--- OBS/001.js
+++ OBS/001.js
@@ -10,8 +10,17 @@ console.log(float)
 console.log(typeof float)
 
 
+
+//string
 var textoComNumero = '532'
 
 console.log(textoComNumero)  // reconhece como numero
 console.log(typeof textoComNumero)  //string
 
+
+
+//Number
+console.log(NaN)
+console.log(typeof NaN)
+console.log(typeof +Infinity)
+console.log(typeof -Infinity)
