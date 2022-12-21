96bcdbde746056cbfda0065cc25897415c6e2b9a
Mateus Henrique <mathmarkub@gmail.com>
2022-12-21 16:34:00 -0300

    Update 018-lower-upper.js

--- OBS/018-lower-upper.js
+++ OBS/018-lower-upper.js
@@ -1,4 +1,5 @@
 // toLowerCase e toUpperCase
+// Caixa baixa e Caixa alta
 
 var frase = "Vamos modidicar esta frase."
 
@@ -7,3 +8,27 @@ var fraseCaps = frase.toUpperCase()
 console.log(fraseCaps)
 
 console.log(frase.toLowerCase())
+
+// trim
+// retira os espaços desnecessários
+
+var nome = "      Mateus Henrique   "
+
+var nomeTrim = nome.trim()
+
+console.log(nome)
+console.log(nomeTrim)
+
+// split
+// separa as palavras em string
+
+var tags = "PHP, JavaScript, Java, HTML, CSS"
+console.log(tags.split(", "))
+
+// lastIndexOf
+// localiza a ultima palavra especifica
+
+var fraseDois = "Eu quero a última palavra teste desta frase de teste"
+
+console.log(fraseDois.indexOf("teste"))
+console.log(fraseDois.lastIndexOf("teste"))
