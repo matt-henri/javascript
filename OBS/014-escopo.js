465a4815e6d8a0e5bec51f55e307ac0fa8422b94
Mateus Henrique <mathmarkub@gmail.com>
2022-12-20 23:17:22 -0300

    Create 014-escopo.js

---
+++ OBS/014-escopo.js
@@ -0,0 +1,11 @@
+
+
+/* 
+
+        - No JavaScript podemos ter vários escopos
+        - O global, que é iniciado em toda a aplicação
+        - E os locais, que podem existir em várias instruções como as funções
+
+*/
+
+
