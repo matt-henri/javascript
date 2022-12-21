053ad38cf0833bc02f961b150aba4e9c720d3821
Mateus Henrique <mathmarkub@gmail.com>
2022-12-21 19:53:30 -0300

    arrays

---
+++ OBS/019-funcao-array-1.js
@@ -0,0 +1,34 @@
+ //    length
+ // conta os caracteres no array
+ 
+ var arr = [1,2,3,4,5]
+ console.log(arr.length)
+
+ // push
+ // acrescenta number ou string
+ 
+ arr.push(6)
+ arr.push('sete')
+
+ console.log(arr)
+
+// pop
+// apaga a ultima inf dentro no array
+arr.pop()
+
+console.log(arr)
+
+// unshit
+// acrescenta algo no começo do array
+arr.unshift(0)
+
+console.log(arr )
+
+// shift
+// apaga alto no começo do array 
+arr.shift()
+console.log(arr)
+ 
+
+// acessar o último elemento do array
+console.log(arr[arr.length - 1])
---
+++ OBS/019-funcao-array-2.js
@@ -0,0 +1,27 @@
+ // splice
+ var arr = [1,2,3,4,5]
+
+ arr.splice(2, 0, 999)
+
+ console.log(arr)
+
+ arr.splice(4, 1)
+
+ console.log(arr)
+
+// indexOf
+// informa algo especifico escolhido dentro do ()
+
+ console.log(arr.indexOf(5))
+
+ // join
+ // unifica todas as inf dentro da string em uma frase
+ var arr2 = ["O", "rato", "roeu", "a", "roupa"]
+
+ console.log(arr2.join(" "))
+ console.log(arr2.join(" , "))
+
+ //reverse
+ // inverte a ordem das strings dentro do array
+ console.log(arr2.reverse())
+
