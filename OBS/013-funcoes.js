564faf088befe46692612339b4c0e147e9f7b1c9
Mateus Henrique <mathmarkub@gmail.com>
2022-12-20 22:57:13 -0300

    Create 013-funcoes.js

---
+++ OBS/013-funcoes.js
@@ -0,0 +1,41 @@
+
+
+/*
+
+    - Funções são blocos de códigos reutilizáveis
+    - Ou seja, evitamos a repetição da lógica de um programa em diversas partes do código
+    - a função precisa ser invocada para ser executada
+
+*/
+
+
+function primeiraFuncao() {
+
+    console.log("Hello World")
+
+}
+
+primeiraFuncao()
+
+function dizerNome(nome) {
+    console.log("O nome é: " + nome)
+}
+
+dizerNome("Mateus")
+dizerNome("Pedro")
+dizerNome("Marcos")
+
+var nomeDoBancoDeDados = "Bruno"
+
+dizerNome(nomeDoBancoDeDados)
+
+function soma(a, b) {
+    var soma = a + b
+    return soma
+}
+
+var somaUm = soma(2, 6)
+console.log(somaUm)
+
+var somaDois = soma(5, 6)
+console.log(somaDois)
