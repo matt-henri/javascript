e1980188604dbe64d42e498baac67fb50f8a80d9
Mateus Henrique <mathmarkub@gmail.com>
2022-12-21 15:20:27 -0300

    Create 016-numericos.js

---
+++ OBS/016-numericos.js
@@ -0,0 +1,22 @@
+// parseFloat
+
+console.log(parseFloat('12.9999'))
+console.log(Number.parseFloat('12.8888'))
+
+// parseInt
+
+console.log(parseInt('10'))
+console.log(parseInt(16.99))
+
+// toFixed
+
+console.log(744.829371.toFixed(1))
+console.log(744.829371.toFixed(2))
+console.log(744.829371.toFixed(3))
+
+// isNaN
+
+console.log(isNaN("teste"))
+console.log(isNaN(12))
+console.log(isNaN("14"))
+
