2c8dedca9c2b85afa1345e0d2a79f61a5728c577
Mateus Henrique <mathmarkub@gmail.com>
2022-12-23 19:42:33 -0300

    att querySelector

---
+++ OBS/021-querySelector/script.js
@@ -0,0 +1,12 @@
+// DOM - Document Object Model
+
+//const p = document.getElementById("texto")
+
+//const p = document.getElementsByTagName("p")
+
+// const p = document.getElementsByClassName("texto")
+
+
+const p = document.querySelector("#texto")
+
+console.log(p)
