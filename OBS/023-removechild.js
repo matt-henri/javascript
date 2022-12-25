40c1f008dea9eeb9f4e3a47ba89909e6acc5fd5d
Mateus Henrique <mathmarkub@gmail.com>
2022-12-25 17:00:32 -0300

    desafio imc

---
+++ OBS/023-removechild.js
@@ -0,0 +1,11 @@
+var container = document.querySelector("#container")
+
+var p = document.querySelector("#container p")
+
+container.removeChild(p)
+
+// remover o elemento
+
+var subtitle = document.querySelector(".subtitle")
+
+subtitle.remove()
