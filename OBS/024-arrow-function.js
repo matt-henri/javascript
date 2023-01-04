501bee9783d096df7184ca5fe6e66f8b1a5d4237
Mateus Henrique <mathmarkub@gmail.com>
2023-01-04 10:33:13 -0300

    Create 024-arrow-function.js

---
+++ OBS/024-arrow-function.js
@@ -0,0 +1,14 @@
+/*const soma = function(v1, v2) { return v1*v2}
+
+const soma = (v1,v2) => { return v1 +v2}
+
+console.log(soma(2,404))
+
+const nome = mateus => {return nome}
+
+console.log(nome()) */
+//)
+
+const soma = (a,b) => a+b
+
+console.log(soma(2,3))
