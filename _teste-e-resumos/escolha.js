026f4024e5a1c3949431436fc9193331c3be26d6
Mateus Henrique <mathmarkub@gmail.com>
2023-01-05 20:22:53 -0300

    Update escolha.js

--- _teste-e-resumos/escolha.js
+++ _teste-e-resumos/escolha.js
@@ -1,14 +1,13 @@
 let nome = "mateus"
-let salario = 1250
-let dependentes = 5
+var salario = 1250
+var dependentes = 7
 
-
-if (dependentes == 0) {
-    console.log(salario + ((salario*5)/100))
-} else if (dependentes >= 1 || dependentes <= 3) {
+if (dependentes <= 0) {
+    console.log(salario + (salario*5)/100)
+} else if (dependentes <= 1 || dependentes <= 3) {
     console.log(salario + ((salario*10)/100))
-} else if (dependentes >= 4 || dependentes <= 6) {
-    console.log(salario + ((salario*15)/100)) 
+} else if ( dependentes <= 4 || dependentes <= 6) {
+    console.log(salario + ((salario*15)/100))
 } else {
     console.log(salario + ((salario*18)/100))
 }
