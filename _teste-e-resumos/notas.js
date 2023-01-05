8f514f733ef5d95f2711a6efd7b9630bd5aa24be
Mateus Henrique <mathmarkub@gmail.com>
2023-01-05 18:58:22 -0300

    Create notas.js

---
+++ _teste-e-resumos/notas.js
@@ -0,0 +1,12 @@
+console.log("Escola Estadual MH")
+
+let PrimeiraNota = 7
+let SegundaNota = 6
+
+let notaFinal = (PrimeiraNota + SegundaNota) / 2
+
+if (notaFinal < 6) {
+    console.log(`Sua média final é de ${notaFinal}, você nao foi aprovado!`)
+} else {
+    console.log(`Sua média final é de ${notaFinal}, parabéns, você foi aprovado!`)
+}
