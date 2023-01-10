950e50f36abb672e0ee50c3efd690a566da78edc
Mateus Henrique <mathmarkub@gmail.com>
2023-01-09 23:41:44 -0300

    Update contdor.js

--- _teste-e-resumos/contdor.js
+++ _teste-e-resumos/contdor.js
@@ -1,5 +1,11 @@
-for (var c = 1; c <= 20; c++) {
+for (var c = 1; c <= 10; c++) {
     console.log(c)
+}
+
+console.log('concluído')
+
+for (var v = 10; v >= 1; v--) {
+    console.log(v)
 }
 
 console.log('concluído')
