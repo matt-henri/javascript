2c23696e7d3a6a1ae8f2fb9200d24fc7eb033c0a
Mateus Henrique <mathmarkub@gmail.com>
2023-01-10 01:23:45 -0300

    Create function-soma.js

---
+++ _function/function-soma.js
@@ -0,0 +1,13 @@
+function somaMais(a, b) {
+    return a + b
+}
+
+console.log(somaMais(10,2))
+console.log(somaMais(19,5))
+
+function somaMenos(a,b) {
+    return a - b
+}
+
+console.log(somaMenos(23, 4))
+console.log(somaMenos(9, 2))
