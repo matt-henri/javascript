f3cbb7572a19361da9f39d37304fbc71c3e878df
Mateus Henrique <mathmarkub@gmail.com>
2023-01-12 14:50:45 -0300

    att

--- _teste-e-resumos/imc.js
+++ _teste-e-resumos/imc.js
@@ -1,14 +1,14 @@
-const peso = 64
-const altura = 1.73
+let altura = 1.73
+let peso = 63
 
-let calc = altura * 2
-let calcFinal = ((peso/calc).toFixed(2))
+let imc = (peso / (altura*altura)).toFixed(1)
 
-if (calcFinal <= 18) {
-    console.log (`seu peso imc é ${calcFinal}, esta abaixo do peso`)
-}else if (calcFinal <= 25 ) {
-    console.log(`seu imc é ${calcFinal}, parabéns, está no peso ideal`)
+if ( imc <= 18) {
+    console.log (`Seu IMC é de ${imc}, vc esta abaixo do peso ideal.`)
+} else if ( imc < 25 ) {
+    console.log (`Seu IMC é de ${imc}, você esta no peso ideia.`)
+} else if ( imc < 30 ) {
+    console.log (`Seu IMC é de ${imc}, você esta levemente acima do peso.`)
 } else {
-    console.log(`seu imc é de ${calcFinal}, vc está acima do peso`)
+    console.log (`Seu IMC é de ${imc}, você esta acima do peso.`)
 }
- 
