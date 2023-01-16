c7dde13bda23195bb72ddae06c8201581ea23f9b
Mateus Henrique <mathmarkub@gmail.com>
2023-01-15 22:36:08 -0300

    arr

---
+++ aula13/arrow-function.js
@@ -0,0 +1,24 @@
+/*           ---     conceito básico    ---
+
+    só pode fazer uma arrow function sem () 
+    quando só tem um parametro
+
+*/
+
+// exemplo que funciona
+
+const sum = number => number * 2
+
+console.log(sum(5))
+
+// exemplo que não funciona
+
+const sumDois = number1, number2 => number1 + number2
+
+console.log(sumDois(2, 2))
+
+// neste caso o arrow function teria que ficar assim
+
+const sumTres = (num1, num2) => num1 + num2
+
+console.log(sumTres(2, 2))
