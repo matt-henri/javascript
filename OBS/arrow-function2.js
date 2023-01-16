7113f9b7b46199be40ad8778195442ec99386078
Mateus Henrique <mathmarkub@gmail.com>
2023-01-15 22:02:01 -0300

    Create arrow-function2.js

---
+++ OBS/arrow-function2.js
@@ -0,0 +1,19 @@
+function sum  (number1, number2) {
+    return number1 + number2
+}
+
+console.log(sum(2, 4))
+
+// incluindo a function em uma variável
+
+const sumDois = function dois(num1, num2) {
+    return num1 + num2
+}
+
+console.log(sumDois(4, 4))
+
+// simplificando em ArrowFunction
+// quando está simplificada, não tem a necessidade de usar 'Return'
+
+const sumTres = (num1, num2) => num1 + num2
+console.log(sumTres(3, 3))
