b1b7b18437fc482af1013534256fa2ae815cc67d
Mateus Henrique <mathmarkub@gmail.com>
2023-01-19 18:15:13 -0300

    Update while.js

--- _COD3R-basico/while.js
+++ _COD3R-basico/while.js
@@ -11,3 +11,9 @@ let contador = 1
         console.log(`testando o contador = ${contador}`)
         contador++
     }
+
+// pode ser feito também como 'for'
+
+for ( let i = 1; i <= 10; i++) {
+    console.log(`numero ${i}`)
+}
